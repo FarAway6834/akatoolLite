18cefc814c5d4bb57653cadf52933f687bb3b161
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2026-02-28 11:55:07 +0900

    Update __main__.py

--- akatool/__main__.py
+++ akatool/__main__.py
@@ -1,2 +1,1 @@
-#incognito('http://codeup.kr/loginpage.php')
-(lambda main : main() if __name__ == "__main__" else main)(lambda : (incognito('http://c.x3.kro.kr'), __import__('subpr').lib.subpr('python')))
+(lambda main : main() if __name__ == "__main__" else main)(lambda : (incognito('http://codeup.kr/loginpage.php'), __import__('subpr').lib.subpr('python')))
